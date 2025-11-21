7497e922306f93a8fd94d083646f68b873203cd5
abble01 <richard.mesioye@gmail.com>
2025-11-21 21:18:11 +0000

    Refactor get_data and update period options
    Updated the get_data function to handle empty data and modified the period options in the selectbox.

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import yfinance as yf
-import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
 
 def get_data(tick, prd):
-    
-    
-    
+     
     data = yf.download(tick, period=prd)
-    
-    
+    if not data:
+        return
+    ...
     
 
 def main():
     st.write("Simple Stock tracker.")
     
     ticker = st.text_input(label= "Enter ticker eg, AAPL")
-    period = st.selectbox(label="Select time period", options=("1d", "1mo", "1y"))
+    period = st.selectbox(label="Select time period", options=("1d", "1mo", "6mo", "1y", "5y"))
     
     if st.button(label="Get Data"):
         get_data
