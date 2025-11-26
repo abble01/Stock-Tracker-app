a9d23ad62e59f20eb81bcb57974cfb62012b59d3
abble <rm99.mesioye@gmail.com>
2025-11-26 21:28:41 +0000

    Further completion

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 import yfinance as yf
-import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.graph_objects as go
 import streamlit as st
+import datetime
 
+#Project 2 - Stock visualiser + more.
 
-def get_data(tick, prd):
-     
-    data = yf.download(tick, period=prd)
-    if not data:
-        return
-    ...
+today = datetime.date.today
+options = ["MSFT", "AAPL", "TSLA", "GOOGL"]
+
+def get_data(tick, prd, interval):
     
+    if tick:
+        data = yf.download(tick, period=prd, interval=interval)
+    if data.empty:
+        return
+    st.write(f"Stock price for {tick} over {prd}")
+    st.line_chart(data["Close"])
+    
+    
+    start_date = data.index[0]
+    end_date = data.index[-1]
+    # add moving average and candlestick graph next session
 
 def main():
-    st.write("Simple Stock tracker.")
+    st.sidebar.header("Stock price visualizer")
     
-    ticker = st.text_input(label= "Enter ticker eg, AAPL")
-    period = st.selectbox(label="Select time period", options=("1d", "1mo", "6mo", "1y", "5y"))
+    ticker = st.sidebar.multiselect(label= "Enter ticker(s) eg, AAPL", options=options)
+    period = st.sidebar.selectbox(label="Select time period", options=("1d", "5d", "1mo", "1y", "6y"))
+    interval = st.sidebar.selectbox(label="Enter the interval you want", options=("2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"))
+    avg = st.sidebar.number_input(label="Enter a moving average", value=9)
     
-    if st.button(label="Get Data"):
-        get_data
     
+    if st.sidebar.button(label="Get Data"):
+        get_data(ticker, period, interval)
+    
+    
+    
+    
+
 if __name__ == "__main__":
     main()
